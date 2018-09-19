6bfacad2f46b678b5ad4db42a9e44569d20d01e3
Luke Vawter <lvawter@icprb.org>
2018-09-19 17:38:15 -0400

    added www/CSS/main.css

--- ui.R
+++ ui.R
@@ -2,6 +2,15 @@ dashboardPage(
   dashboardHeader(title = "take2",
                   tags$li("variable forthcoming",
                   class = "dropdown")),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardSidebar(tags$li("this is the sidebar"),
+                   dateInput("date", "enter a date")),
+  dashboardBody(    tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "CSS/main.css")
+  ),
+
+    tags$li("this is a test document...like the many before it ...and teh many to come...")#,
+  
+    #sliderInput(inputId = "my_slider",label = "test", min = 1,max = 10, value = 5)
+  )
+  
 )
