13feeb877fa714f415091c3b8ef1b78d5cd01c37
ShyGuyPy <luke.vawter1@gmail.com>
2018-09-21 16:01:36 -0400

    separate divs for quads, date font size increased

--- ui.R
+++ ui.R
@@ -12,56 +12,61 @@ dashboardPage(
   
     #sliderInput(inputId = "my_slider",label = "test", min = 1,max = 10, value = 5)
   ,
-  div(class="topbox",
-      div(class="square",
-          div(class="content",
-              div(class="table",
-                  div(class="table-cell",
-                      p("precipitation deficit")
-                  )))),
-      div(class="square",
-          div(class="content",
-              div(class="table",
-                  div(class="table-cell",
-                      p("ground water wells")
-                  )))),
-      div(class="square",
-          div(class="content",
-              div(class="table",
-                  div(class="table-cell",
-                      p("reservoir flow")
-                  )))),
-      div(class="square",
-          div(class="content",
-              div(class="table",
-                  div(class="table-cell",
-                      p("streamflow")
-                  )))),
-      div(class="square",
-          div(class="content",
-              div(class="table",
-                  div(class="table-cell",
-                      p("precipitation deficit")
-                  )))),
-      div(class="square",
-          div(class="content",
-              div(class="table",
-                  div(class="table-cell",
-                      p("ground water wells")
-                  )))),
-      div(class="square",
-          div(class="content",
-              div(class="table",
-                  div(class="table-cell",
-                      p("reservoir flow")
-                  )))),
-      div(class="square",
-          div(class="content",
-              div(class="table",
-                  div(class="table-cell",
-                      p("streamflow")
-                  ))))
-  ) #end of topbox 
+  div(class="topbox_main",
+    div(class="topbox1",
+        div(class="square",
+            div(class="content",
+                div(class="table",
+                    div(class="table-cell",
+                        p("precipitation deficit")
+                    )))), 
+        div(class="square",
+            div(class="content",
+                div(class="table",
+                    div(class="table-cell",
+                        p("ground water wells")
+                    )))),
+        div(class="square",
+            div(class="content",
+                div(class="table",
+                    div(class="table-cell",
+                        p("reservoir flow")
+                    )))),
+        div(class="square",
+            div(class="content",
+                div(class="table",
+                    div(class="table-cell",
+                        p("streamflow")
+                    ))))
+    ), #end of topbox1
+    div(class="topbox2",
+        div(class="square",
+            div(class="content",
+                div(class="table",
+                    div(class="table-cell",
+                        p("precipitation deficit")
+                    )))),
+        div(class="square",
+            div(class="content",
+                div(class="table",
+                    div(class="table-cell",
+                        p("ground water wells")
+                    )))),
+        div(class="square",
+            div(class="content",
+                div(class="table",
+                    div(class="table-cell",
+                        p("reservoir flow")
+                    )))),
+        div(class="square",
+            div(class="content",
+                div(class="table",
+                    div(class="table-cell",
+                        p("streamflow")
+                    ))))
+    ) #end of topbox2
+  ) #end of topbox-main
+    
   )
   
 )
