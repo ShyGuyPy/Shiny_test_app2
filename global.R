d111ccbc8f62ad5d0d03bd28979c817490b31aaf
Luke Vawter <lvawter@icprb.org>
2018-10-15 10:00:19 -0400

    fixed typo in directory name.  Path seems to work now

--- global.R
+++ global.R
@@ -15,23 +15,23 @@ black <- "background-color: black"
 
 
 
-path_p <- "C:\\Users\\icprbadmin\\Documents\\R\\2018drex\\input\\ts\\state\\i_a1b\\va_shenandoah_p.csv"
-path_q <- "C:\\Users\\icprbadmin\\Documents\\R\\2018drex\\input\\ts\\state\\i_a1b\\va_shenandoah_q.csv"
-path_s <- "C:\\Users\\icprbadmin\\Documents\\R\\2018drex\\input\\ts\\state\\i_a1b\\va_shenandoah_stor.csv"
-path_g <- "C:\\Users\\icprbadmin\\Documents\\R\\2018drex\\input\\ts\\state\\i_a1b\\va_shenandoah_gw.csv"
+# path_p <- "C:\\Users\\icprbadmin\\Documents\\R\\2018drex\\input\\ts\\state\\i_a1b\\va_shenandoah_p.csv"
+# path_q <- "C:\\Users\\icprbadmin\\Documents\\R\\2018drex\\input\\ts\\state\\i_a1b\\va_shenandoah_q.csv"
+# path_s <- "C:\\Users\\icprbadmin\\Documents\\R\\2018drex\\input\\ts\\state\\i_a1b\\va_shenandoah_stor.csv"
+# path_g <- "C:\\Users\\icprbadmin\\Documents\\R\\2018drex\\input\\ts\\state\\i_a1b\\va_shenandoah_gw.csv"
+# 
+# #for mac use
+# if( .Platform$OS.type == "unix" ) {
+#   path_p <- "/Users/lukevawter/Desktop/R/2018drex/input/ts/state/i_a1b/va_shenandoah_p.csv"
+#   path_q <- "/Users/lukevawter/Desktop/R/2018drex/input/ts/state/i_a1b/va_shenandoah_q.csv"
+#   path_s <- "/Users/lukevawter/Desktop/R/2018drex/input/ts/state/i_a1b/va_shenandoah_stor.csv"
+#   path_g <- "/Users/lukevawter/Desktop/R/2018drex/input/ts/state/i_a1b/va_shenandoah_gw.csv"
+# }
 
-#for mac use
-if( .Platform$OS.type == "unix" ) {
-  path_p <- "/Users/lukevawter/Desktop/R/2018drex/input/ts/state/i_a1b/va_shenandoah_p.csv"
-  path_q <- "/Users/lukevawter/Desktop/R/2018drex/input/ts/state/i_a1b/va_shenandoah_q.csv"
-  path_s <- "/Users/lukevawter/Desktop/R/2018drex/input/ts/state/i_a1b/va_shenandoah_stor.csv"
-  path_g <- "/Users/lukevawter/Desktop/R/2018drex/input/ts/state/i_a1b/va_shenandoah_gw.csv"
-}
-
-# path_p <- paste(ts_path,"va_shenandoah_p.csv")
-# path_q <- paste0(ts_path,"va_shenandoah_q.csv")
-# path_s <- paste0(ts_path,"va_shenandoah_stor.csv")
-# path_g <- paste0(ts_path,"va_shenandoah_gw.csv")
+path_p <- paste0(ts_path,"va_shenandoah_p.csv")
+path_q <- paste0(ts_path,"va_shenandoah_q.csv")
+path_s <- paste0(ts_path,"va_shenandoah_stor.csv")
+path_g <- paste0(ts_path,"va_shenandoah_gw.csv")
 # 
 my_data_p = fread(path_p)
 my_data_q = fread(path_q)
